b934ab9274771f92a10d7f18257e6fe0f5ca24a4
Ns2Kracy <ns2kracy@gmail.com>
2025-05-07 11:55:08 +0800

    feat: metadata provider trait

--- crates/ayiah-provider/src/lib.rs
+++ crates/ayiah-provider/src/lib.rs
@@ -1,5 +1,36 @@
+use std::error::Error;
+
+use serde::{Deserialize, Serialize};
+
 pub mod anilist;
 pub mod bangumi;
 pub mod douban;
 pub mod tmdb;
 pub mod tvdb;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct VideoMetadata {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct BookMetadata {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct MusicMetadata {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct ComicMetadata {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "type")]
+enum MediaMetadata {
+    Video(VideoMetadata),
+    Book(BookMetadata),
+    Music(MusicMetadata),
+    Comic(ComicMetadata),
+}
+
+pub trait MetadataProvider {
+    fn fetch_metadata(
+        &self,
+    ) -> impl std::future::Future<Output = Result<MediaMetadata, Box<dyn Error>>> + Send;
+}
