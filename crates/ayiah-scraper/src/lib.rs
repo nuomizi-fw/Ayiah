e699fe2146fd136f073bf14d44c3e9c973399f0e
Ns2Kracy <ns2kracy@gmail.com>
2025-05-30 17:25:52 +0800

    feat: implement initial scraper module with metadata providers and media types

--- crates/ayiah-scraper/src/lib.rs
+++ crates/ayiah-scraper/src/lib.rs
@@ -1,1 +1,28 @@
+mod provider;
 
+use provider::MetadataProvider;
+
+pub enum OrganizeMethod {
+    SoftLink,
+    HardLink,
+    Copy,
+    Cut,
+}
+
+pub struct AyiahScraper {
+    pub organize_method: OrganizeMethod,
+    pub source_path: String,
+    pub target_path: String,
+    pub metadata_provider: Option<Box<dyn MetadataProvider>>,
+}
+
+impl AyiahScraper {
+    pub fn new(organize_method: OrganizeMethod, source_path: String, target_path: String) -> Self {
+        Self {
+            organize_method,
+            source_path,
+            target_path,
+            metadata_provider: None,
+        }
+    }
+}
